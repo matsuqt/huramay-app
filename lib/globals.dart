efc8e352ea7c916a54f5cfea042d8ce1b4667c9f
Matthew Valila <matthewvalila03@gmail.com>
2026-05-13 03:25:35 +0800

    Updated inen na globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -2,4 +2,4 @@
 Map<String, dynamic>? currentUser;
 
 // Set to your LIVE server so the APK works on your physical phone
-const String baseUrl = 'http://192.168.137.1:5000';
+const String baseUrl = 'http://10.198.13.39:5000';
